3c5b3dacc1276b21f78b4051a01b14a82e75525c
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-08-02 11:45:41 -0400

    make the name, email, and phonenumber fields mass fillable in the customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -19,7 +19,7 @@ class Customer extends Model
     //protected $primaryKey = 'id';
     // public $timestamps = false;
     // protected $guarded = ['id'];
-    // protected $fillable = [];
+    protected $fillable = ['name', 'email', 'phone_number'];
     // protected $hidden = [];
     // protected $dates = [];
 
