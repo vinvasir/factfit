41c3b8ab908d58614efe7d95c9a6419d77000184
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-08-02 11:44:09 -0400

    set up the route group for admin crud resources. add a specific route for the customers resource

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Admin group
+Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function() {
+    CRUD::resource('customer', 'Admin\CustomerCrudController');
+});
