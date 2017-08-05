d4fe0ded906ffff08038b3fe4e4deab5301187f5
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-08-05 13:43:08 -0400

    add a DayTest file

---
+++ tests/Unit/DayTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ThreadTest extends TestCase
+{
+		use DatabaseMigrations;
+
+	  protected $day;
+
+		public function setUp()
+		{
+			parent::setUp();
+			$this->day = factory('App\Day')->create();
+		}
+
+    /** @test */
+    function a_day_can_set_food_goal_progress()
+    {
+
+    }
+}
