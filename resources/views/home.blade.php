ce2e60f4ab82ea4997f7fe853de90fd4ee1cda2e
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-08-05 16:05:15 -0400

    give the user more feedback on the ladning page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,17 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    @if(Auth::check())
+
+                    <h3>Successfully logged in!</h3>
+
+                    <p><a href="/app">Click here</a> to start recording your fitness activity</p>
+
+                    @else
+
+                    <h3>Welcome to Factfit! Please log in to continue.</h3>
+
+                    @endif
                 </div>
             </div>
         </div>
