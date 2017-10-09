6baaf16da845be7cf0c630f47034bc574cfbb3aa
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-10-09 15:23:31 -0400

    start implementing the calendar event interfact for days

--- app/Day.php
+++ app/Day.php
@@ -3,34 +3,67 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use MaddHatter\LaravelFullcalendar\Event as FCEvent;
 
-class Day extends Model
+class Day extends Model implements FCEvent
 {
-	protected $fillable = ['user_id', 'date', 'weight', 'good_food_count', 'bad_food_count'];
+  protected $fillable = ['user_id', 'date', 'weight', 'good_food_count', 'bad_food_count'];
 
-    protected $with = ['foods'];
+  protected $with = ['foods'];
 
-	public function user()
-	{
-		return $this->belongsTo(User::class);
-	}
+  protected $carbonizedDate = null;
 
-    public function foods()
-    {
-        return $this->hasMany(Food::class);
-    }
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 
-    public function setProgress()
-    {
-		$food_progress = ($this->good_food_count - $this->bad_food_count) / 10;
+  public function foods()
+  {
+    return $this->hasMany(Food::class);
+  }
+
+  public function setProgress()
+  {
+    $food_progress = ($this->good_food_count - $this->bad_food_count) / 10;
         
-		$this->food_goal_progress = $food_progress;
-// dd($this->good_food_count);
-		$this->save();
+    $this->food_goal_progress = $food_progress;
+
+    $this->save();
+  }
+
+  public function addFood($requestParams)
+  {
+    $this->foods()->create($requestParams);
+  }
+
+  public function getTitle()
+  {
+    return $this->getCarbonizedDate()->toFormattedDateString();
+  }
+
+   public function isAllDay() 
+  {
+     return true;
+  }
+
+  public function getStart()
+  {
+    return $this->getCarbonizedDate()->toDateTimeString();
+  }
+
+  public function getEnd()
+  {
+    return Carbon::midnight();
+  }
+
+  protected function getCarbonizedDate()
+  {
+    if (!$this->carbonizedDate && $this->date) {
+      $this->carbonizedDate = new Carbon($this->date);
     }
 
-    public function addFood($requestParams)
-    {
-		$this->foods()->create($requestParams);
-    }
+    return $this->carbonizedDate;
+  }
 }
