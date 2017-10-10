698a2d252345258dda569ca7e78bdb3654795e56
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-10-10 14:51:15 -0400

    Write a failing test for unpermitted User Settings

--- tests/Feature/ManageUsersTest.php
+++ tests/Feature/ManageUsersTest.php
@@ -72,5 +72,40 @@ class ManageUsersTest extends TestCase
   			->assertRedirect('/login');
 
   	$this->assertNotEquals($settings, $otherUser->fresh()->settings);  	
+  }
+
+  /** @test */
+  function an_authenticated_user_may_only_update_privacy_and_theme_settings()
+  {
+    $this->signIn();
+
+    $settings = ['privacy' => [
+        'public' => false,
+        'showWeightTo' => ['friends', 'followedUsers'],
+        'showFoodProgressTo' => ['followers']
+      ],
+      'appTheme' => [
+        'backgroundColor' => 'dark'
+      ],
+      'permissions' => [
+        'admin' => true
+      ]
+    ];
+
+    $this->post('/app/users/my-settings', ['settings' => $settings]);
+
+    $this->assertNotEquals($settings, auth()->user()->fresh()->settings);
+
+    $expectedUserSettings =['privacy' => [
+        'public' => false,
+        'showWeightTo' => ['friends', 'followedUsers'],
+        'showFoodProgressTo' => ['followers']
+      ],
+      'appTheme' => [
+        'backgroundColor' => 'dark'
+      ]
+    ];
+
+    $this->assertEquals($expectedUserSettings, auth()->user()->fresh()->settings);
   }  
 }    
