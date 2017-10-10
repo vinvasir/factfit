8e766f98d262332f49501a269e0afaaaadb770bf
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-10-10 14:07:45 -0400

    Add a ManageUsersTest

---
+++ tests/Feature/ManageUsersTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ManageUsersTest extends TestCase
+{
+  use DatabaseTransactions;
+
+    /** @test */
+    function a_user_may_update_their_settings()
+    {
+
+    }
+}    
