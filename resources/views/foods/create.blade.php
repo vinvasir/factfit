dd92c6765df67a2df6e773e61ca34d825dd8d8fb
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-10-26 10:28:51 -0400

    Use CSS tricks/stack-overflow circle form

--- resources/views/foods/create.blade.php
+++ resources/views/foods/create.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
 <div class="container">
+    <ul class='circle-container'>
+      <li><img src='http://lorempixel.com/100/100/city' alt="..." /></li>
+      <li><img src='http://lorempixel.com/100/100/nature' alt="..." /></li>
+      <li><img src='http://lorempixel.com/100/100/abstract' alt="..." /></li>
+      <li><img src='http://lorempixel.com/100/100/cats' alt="..." /></li>
+      <li><img src='http://lorempixel.com/100/100/food' alt="..." /></li>
+      <li><img src='http://lorempixel.com/100/100/animals' alt="..." /></li>
+      <li><img src='http://lorempixel.com/100/100/business' alt="..." /></li>
+      <li><img src='http://lorempixel.com/100/100/people' alt="..." /></li>
+    </ul>
+    
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
