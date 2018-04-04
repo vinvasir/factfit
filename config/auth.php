3195b1160f6a367089b91ac7a98842e0361ecc8e
Prateek Vasireddy <prat.vasireddy@gmail.com>
2018-04-04 00:58:16 -0400

    Revert "Revert "install canvas for blogging""
    This reverts commit 049ddb4dc499c5dfb64a9bd6d34cf46fbf40492a.

--- config/auth.php
+++ config/auth.php
@@ -45,6 +45,11 @@ return [
             'driver' => 'token',
             'provider' => 'users',
         ],
+        
+        'canvas' => [
+            'driver' => 'session',
+            'provider' => 'canvas_users',
+        ],        
     ],
 
     /*
@@ -68,6 +73,11 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\User::class,
+        ],
+
+        'canvas_users' => [
+            'driver' => 'eloquent',
+            'model' => \Canvas\Models\User::class,
         ],
 
         // 'users' => [
@@ -97,6 +107,13 @@ return [
             'table' => 'password_resets',
             'expire' => 60,
         ],
+
+        'canvas_users' => [
+            'provider' => 'canvas_users',
+            'email' => 'auth.emails.password',
+            'table' => 'password_resets',
+            'expire' => 60,
+        ],        
     ],
 
 ];
